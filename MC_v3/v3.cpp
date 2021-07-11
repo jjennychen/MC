cc86fa3d7200d67bb07b56c9d925e54eb6c15ee5
Jenny Chen <jjenny0503@gmail.com>
2021-07-11 10:48:10 -0400

    update :)

--- MC_v3/v3.cpp
+++ MC_v3/v3.cpp
@@ -7,8 +7,6 @@
 #include <fstream>
 #include <fstream>
 
-//
-
 using namespace std;
 
 void ascii(map<string, string> &codeData)
@@ -52,7 +50,7 @@ void ascii(map<string, string> &codeData)
     codeData["0"] = "-----";
 }
 
-void read(string filename, string &sentence, bool &invalid)
+void read(string filename, bool &invalid, vector<string> &sentence_list)
 {
     ifstream file (filename);
     if (file.good())
@@ -61,7 +59,7 @@ void read(string filename, string &sentence, bool &invalid)
         string input;
         while (getline(file, input))
         {
-            sentence += input; //use encode method here for each input
+            sentence_list.push_back(input); //use encode method here for each input
         }
     }
     else
@@ -83,14 +81,6 @@ void encode(bool &main_page)
         //get a use input
         cout << "Please enter the file name to convert (press <ENTER> to exit or enter \"MAIN\" to go back to the main page): ";
         getline(cin, filename, '\n');
-        // try
-        // {
-        //     if (std::ifstream file (filename))
-        // }
-        // catch(const std::exception& e)
-        // {
-        //     std::cerr << e.what() << '\n';
-        // }
         
         if (filename == "")
         {
@@ -104,53 +94,55 @@ void encode(bool &main_page)
         }
         else
         {
+            vector<string> sentence_list;
+
             //read file and see if it works
             bool invalid = false;
-            read(filename, sentence, invalid);
+            read(filename, invalid, sentence_list);
             if (invalid == true) continue;
 
             //create a output file
             ofstream output ((filename + "_output").c_str(), ios::app);
 
-            //check if the string contains only alphabets/numbers/spaces(no special chars)
-            bool b = true;
-            char letter_check;
-            for (int i = 0; i < sentence.length(); i++)
+            for (string sentence : sentence_list)
             {
-                letter_check = sentence[i];
-                if (!(isalpha(letter_check) || isdigit(letter_check) || isspace(letter_check) || letter_check == '\n'))
-                {
-                    b = false;
-                    break;
-                }
-            }
-
-            //convert the message (if contains special characters -> print "Please enter...")
-            if (b)
-            {
-                char letter;
+                //check if the string contains only alphabets/numbers/spaces(no special chars)
+                bool b = true;
+                char letter_check;
                 for (int i = 0; i < sentence.length(); i++)
                 {
-                    letter = sentence[i];
-                    if (isalpha(letter) || isdigit(letter))
+                    letter_check = sentence[i];
+                    if (!(isalpha(letter_check) || isdigit(letter_check) || isspace(letter_check)))
                     {
-                        string key;
-                        key = tolower(sentence[i]);
-                        output << codeData[key] + " ";
-                    }
-                    else if (isspace(letter))
-                    {
-                        output << "/ ";
-                    }
-                    else if (letter == '\n')
-                    {
-                        output << "\n";
+                        b = false;
+                        break;
                     }
                 }
-            }
-            else
-            {
-                cout << "The file contains un-convertable characters. Please try again." << endl;
+
+                //convert the message (if contains special characters -> print "Please enter...")
+                if (b)
+                {
+                    char letter;
+                    for (int i = 0; i < sentence.length(); i++)
+                    {
+                        letter = sentence[i];
+                        if (isalpha(letter) || isdigit(letter))
+                        {
+                            string key;
+                            key = tolower(sentence[i]);
+                            output << codeData[key] + " ";
+                        }
+                        else if (isspace(letter))
+                        {
+                            output << "/ ";
+                        }
+                    }
+                    output << "\n";
+                }
+                else
+                {
+                    cout << "The file contains un-convertable characters. Please try again." << endl;
+                }
             }
         }
     }
@@ -161,74 +153,88 @@ void decoder(bool &main_page)
     map<string, string> codeData;
     ascii(codeData);
     string sentence;
+    string filename;
+
     while (true)
     {
         //get a user input
         cout << "Please enter the morse code(press <ENTER> to exit or enter \"MAIN\" to go back to the main page): ";
-        getline(cin, sentence, '\n');
-        if (sentence == "")
+        getline(cin, filename, '\n');
+        if (filename == "")
         {
             main_page = false;
             cout << "Thank you! Bye!" << endl;
             break;
         }
-        else if (sentence == "MAIN")
+        else if (filename == "MAIN")
         {
             break;
         }
         else
         {
-            stringstream input(sentence);
+            vector<string> sentence_list;
 
-            //split the input and put the morse codes into a vector
-            vector<string> code;
-            string word;
+            //read file and see if it works
+            bool invalid = false;
+            read(filename, invalid, sentence_list);
+            if (invalid == true) continue;
+            //create a output file
+            ofstream output ((filename + "_output").c_str(), ios::app);
 
-            while (getline(input, word, ' '))
+            for (string sentence : sentence_list)
             {
-                code.push_back(word);
-            }
+                stringstream input(sentence);
 
+                //split the input and put the morse codes into a vector
+                vector<string> code;
+                string word;
 
-            //check if the string contains only alphabets/numbers/spaces(no special chars)
-            bool b = true;
-            char letter_check;
-            for (int i = 0; i < sentence.length(); i++)
-            {
-                letter_check = sentence[i];
-                if (!(letter_check == '-' || letter_check == '/'|| letter_check == '.' || isspace(letter_check)))
+                while (getline(input, word, ' '))
                 {
-                    b = false;
-                    break;
+                    code.push_back(word);
                 }
-            }
 
-            //convert the message (if contains special characters -> print "Please enter...")
-            if (b)
-            {
-                for (string str : code)
+
+                //check if the string contains only alphabets/numbers/spaces(no special chars)
+                bool b = true;
+                char letter_check;
+                for (int i = 0; i < sentence.length(); i++)
                 {
-                    if (str == "/")
+                    letter_check = sentence[i];
+                    if (!(letter_check == '-' || letter_check == '/'|| letter_check == '.' || isspace(letter_check)))
                     {
-                        cout << " ";
+                        b = false;
+                        break;
                     }
-                    else
+                }
+
+                //convert the message (if contains special characters -> print "Please enter...")
+                if (b)
+                {
+                    for (string str : code)
                     {
-                        for (auto codeMap : codeData)
+                        if (str == "/")
                         {
-                            if (codeMap.second == str)
+                            cout << " ";
+                        }
+                        else
+                        {
+                            for (auto codeMap : codeData)
                             {
-                                cout << codeMap.first;
+                                if (codeMap.second == str)
+                                {
+                                    output << codeMap.first;
+                                }
+                            
                             }
-                        
                         }
                     }
+                    output << endl;
+                    }
+                else
+                {
+                    cout << "Please enter a valid morse code. Than you." << endl;
                 }
-                cout << endl;
-                }
-            else
-            {
-                cout << "Please enter a valid morse code. Than you." << endl;
             }
         }
     }
